dc776b258f156f77addfedc3f8e8bde026464c3a
Karl Jerrenny Ojeda <karlojeda@yahoo.com>
2019-10-13 04:40:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,7 +14,7 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   monopolyHash = base_hash
-  monopolyHash[:railroads][:pieces] =
+  monopolyHash[:railroads].
 end
 
 def monopoly_with_third_tier
