9712dc680c28022436462b3f1445a70bdb4b7279
Karl Jerrenny Ojeda <karlojeda@yahoo.com>
2019-10-13 04:21:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,5 +1,5 @@
 def base_hash
-  newBaseHash{:name} => "Karl"
+  newBaseHash[:name] => "Karl"
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
 	{}
